4294909a694ab5591097c2ea421faa98eba2d3ec
Jacob Remedy RJR <Jacob.Remedy@one.verizon.com>
2016-12-02 17:45:50 -0800

    first commit

--- src/main/java/HelloWorld/App.java
+++ src/main/java/HelloWorld/App.java
@@ -9,5 +9,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        
+        
+        System.out.println( "WE LOVE YOU AND WISH YOU A GREAT DAY!!! :)" );
     }
 }
